7cafca62710b706a9392813f51484ae70b6a8c67
Simon Liang <no.lhr0909@gmail.com>
2022-03-12 00:31:25 +0800

    consuming the languages field

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use std::str::FromStr;
 use lambda_runtime::{service_fn, Error, LambdaEvent};
-use lingua::{Language, LanguageDetectorBuilder};
+use lingua::{Language, LanguageDetectorBuilder, IsoCode639_1};
 use serde_json::{json, Value};
 
 #[tokio::main]
@@ -16,7 +17,15 @@ async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let body: Value = serde_json::from_str(body_json).unwrap();
     let text = body["text"].as_str().unwrap();
 
-    let detector = LanguageDetectorBuilder::from_all_languages().build();
+    // FIXME: make languages optional so it will default to use all languages
+    let languages_json = body["languages"].as_array().unwrap();
+
+    let languages: Vec<Language> = languages_json.iter()
+        .map(|language| IsoCode639_1::from_str(language.as_str().unwrap()).unwrap())
+        .map(|iso_code| Language::from_iso_code_639_1(&iso_code))
+        .collect();
+
+    let detector = LanguageDetectorBuilder::from_languages(&languages).build();
     let confidence_values: Vec<(Language, f64)> = detector.compute_language_confidence_values(text);
 
     let return_values: Vec<Value> = confidence_values
